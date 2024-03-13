2260c24ae233ad7e4b8bed570d85db9384a3de40
Eduardo <eduardo.luiz.s@hotmail.com>
2024-03-12 22:47:42 -0300

    first-commit

---
+++ section5-structs-project/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	title, content, err := getNoteData()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func getNoteData() (string, string, error) {
+	title, err := getUserInput("Note title:")
+
+	if err != nil {
+		return "", "", err
+	}
+
+	content, err := getUserInput("Note content:")
+
+	if err != nil {
+		return "", "", err
+	}
+
+	return title, content, nil
+}
+
+func getUserInput(promp string) (string, error) {
+	fmt.Print(promp)
+	var value string
+	fmt.Scanln(&value)
+
+	if value == "" {
+		return "", errors.New("Invalid input.")
+	}
+	return value, nil
+}
