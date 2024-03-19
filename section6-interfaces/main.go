c3cad0a57f730ac68b835a16c73cf28d1bdbcdff
Eduardo Silva <eduardo.luiz.s@hotmail.com>
2024-03-18 22:19:42 -0300

    feat: using-the-interface

--- section6-interfaces/main.go
+++ section6-interfaces/main.go
@@ -33,27 +33,30 @@ func main() {
 	}
 
 	todo.Display()
-	err = todo.Save()
+	err = saveData(todo)
 
 	if err != nil {
-		fmt.Println("Saving the todo failed.")
+		return
 	}
 
-	fmt.Println("Saving the todo succeeded.")
-
 	userNote.Display()
-	err = userNote.Save()
+	err = saveData(userNote)
+
+	if err != nil {
+		return
+	}
+}
+
+func saveData(data saver) error {
+	err := data.Save()
 
 	if err != nil {
 		fmt.Println("Saving the note failed.")
+		return err
 	}
 
 	fmt.Println("Saving the note succeeded.")
-
-}
-
-func saveData() {
-
+	return nil
 }
 
 func getNoteData() (string, string) {
