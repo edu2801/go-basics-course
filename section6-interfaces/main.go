d86e542a836e6f18efc86327582b07fb79e413d5
Eduardo Silva <eduardo.luiz.s@hotmail.com>
2024-03-22 20:41:05 -0300

    feat: introduction-to-generics

--- section6-interfaces/main.go
+++ section6-interfaces/main.go
@@ -54,6 +54,13 @@ func main() {
 	}
 
 	outputData(userNote)
+
+	result := add(1, 2)
+	fmt.Println(result)
+}
+
+func add[T int | float64 | string](a, b T) T {
+	return a + b
 }
 
 func printSomething(value interface{}) {
